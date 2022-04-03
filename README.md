4330f58b9fc27dcf9dcf0533c10281158224b867
hgarg242 <hgarg242@gmail.com>
2022-04-03 23:41:11 +0530

    update Read Me

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 ## First React app
+
+
+To Do list app . Stores items in broweser local storage
+
+#### deployed at github pages
+
+https://harshit3101.github.io/reactToDoApp/
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import TodoContainer from "./components/TodoContainer";
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Error from "./components/Error";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
@@ -11,13 +11,13 @@ const  rootEle = createRoot(document.getElementById('root'));
 
 rootEle.render(
 <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
         <Navbar />
         <Routes>
             <Route path="/" element={ <TodoContainer/> } />
             <Route path="about" element={ <About/> } />
             <Route path="*" element={ <Error/> } />
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
 </React.StrictMode>
 )
